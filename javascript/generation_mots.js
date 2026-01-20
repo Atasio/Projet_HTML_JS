3f8fe8755ab987ad4aaf8108e6b4c9a423616131
LATXAGUE Thibault <tlatxague@24p001-23.pedago.ipb.fr>
2026-01-20 17:13:56 +0100

    Ajout motDansTableau qui supprime mot si trouve

--- javascript/generation_mots.js
+++ javascript/generation_mots.js
@@ -2,9 +2,23 @@ window.onload = function () {
     const mot = document.getElementById('mot');
 
     mot.addEventListener('input', checkMot);
-    console.log("dqzdqzdq");
 }
+
+var mots = ['test', 'jouer','piscine'];
 
 function checkMot() {
     console.log(mot.value);
+    motDansTableau(mot.value);
+}
+
+function motDansTableau(mot) {
+    if(mots.includes(mot)){
+        console.log(mots);
+        console.log('MOT TROUVE');
+        var indexMot = mots.indexOf(mot);
+        mots.splice(indexMot, 1);
+        console.log(mots);    
+    } else {
+        console.log('MOT NON TROUVE');
+    }
 }
