66ed07a686da1f6287ad48ceb47e195027d4a3b7
Loris OBRY <loris.obry@bordeaux-inp.fr>
2026-01-20 17:42:33 +0100

    fetch words json and generate random one

--- javascript/generation_mots.js
+++ javascript/generation_mots.js
@@ -1,10 +1,34 @@
 window.onload = function () {
+    const words = fetch('../words.json').then(words => words.json());
+    const maxLength = document.getElementById('longueur')
+    const wordToType = document.getElementById('liste_mots');
+    
     const mot = document.getElementById('mot');
-
     mot.addEventListener('input', checkMot);
-    console.log("dqzdqzdq");
+    genererateWords(words, maxLength, wordToType);
+}
+
+async function genererateWords(wordsPromise, maxLength, wordListToAdd) {
+    const words = await wordsPromise;
+    while (true) {
+        const randomWord = generateRandomWord(maxLength, words);
+        wordListToAdd.textContent += " " + randomWord;
+        await sleep(2000);
+        console.log(wordListToAdd);
+    }
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 function checkMot() {
     console.log(mot.value);
+}
+
+function generateRandomWord(maxLength, words) {
+    const filteredWords = words.filter(word => word.length <= maxLength.value);
+    const randomIndex = Math.floor(Math.random() * filteredWords.length);
+    return filteredWords[randomIndex];
+
 }
